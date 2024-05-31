32aee4269bb335cb25c1dba1c5fdcdadc91945f2
Al1Delfan1 <aliidelfani@gmail.com>
2024-05-31 11:11:15 +0330

    Delete s03 directory

--- s03/HT/1. reverse number.py
+++
@@ -1,6 +0,0 @@
-for x in range(5, 0, -1):
-    list = ''
-    for y in range(0,x):
-        list += str(x) + '' 
-        x -= 1
-print(list)
--- s03/a1.py
+++
@@ -1,9 +0,0 @@
-a = float(input())
-b = float(input())
-c = float(input())
-
-x1 = (-b+(b**2-4*a*c)**0.5)/(2*a)
-x2 = (-b-(b**2-4*a*c)**0.5)/(2*a)
-
-print(x1)
-print(x2)
